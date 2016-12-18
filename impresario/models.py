85026ae3e47b92cc266132e9cc42e76c9b512a6f
Piotr Dobrysiak <sip.nestor@gmail.com>
2016-12-18 21:56:55 +0100

    First model Person

--- impresario/models.py
+++ impresario/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    """
+    Person model.
+    """
+    first_name = models.CharField('Imię', max_length=32)
+    last_name = models.CharField('Nazwisko', max_length=32)
+
+    class Meta:
+        app_label = "impresario"
+        verbose_name = 'osoba'
+        verbose_name_plural = 'osoby'
