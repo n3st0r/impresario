ef8a7d079cd6538f3ceef751fcbd91719f3e5e50
Piotr Dobrysiak <p.dobrysiak@multimedia.pl>
2016-12-19 16:02:11 +0100

    Now tests use fixtures.

--- impresario/tests/test_models.py
+++ impresario/tests/test_models.py
@@ -8,25 +8,17 @@
 from django.test import TestCase
 from impresario.models import Person
 import crm.tools as tools
+import json
 
 
 class TestImprasario(TestCase):
-
-    def setUp(self):
-        self.prepare_persons(True)
+    fixtures = ['persons.json', ]
 
     def test_person(self):
-        for data in self.persons:
-            o_person = Person.objects.get(first_name=data[0])
-            self.assertEqual(o_person.first_name, data[0])
-            self.assertEqual(o_person.last_name, data[1])
-
-    def prepare_persons(self, generate_fixtures=False):
-
-        for dana in self.persons:
-            o_person, junk = Person.objects.get_or_create(first_name=dana[0], last_name=dana[1])
-            o_person.save()
-        if generate_fixtures:
-            tools.generate_fixtures(filename='persons.json', app='impresario.Person')
-
-    persons = ['Johny', 'Walker']
+        with open("impresario/fixtures/persons.json") as file:
+            json_data = json.load(file)
+            file.close()
+            for data in json_data:
+                o_person = Person.objects.get(pk=data['pk'])
+                self.assertEqual(o_person.first_name, data['fields']['first_name'])
+                self.assertEqual(o_person.last_name, data['fields']['last_name'])
