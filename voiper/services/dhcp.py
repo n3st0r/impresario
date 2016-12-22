854f9c6f85881684e42f328a60e03ed6e1d86e1b
Piotr Dobrysiak <piotr.dobrysiak@gmail.com>
2016-12-22 15:47:26 +0100

    Added hosts config generator for DHCPd.

--- voiper/services/dhcp.py
+++ voiper/services/dhcp.py
@@ -1,21 +1,46 @@
 from voiper.models.devices import Device
+from netaddr import EUI, mac_unix_expanded
 
 
 def write_file(filename):
-    devices = get_devices()
     with open(filename, 'w') as file:
-        for device in devices:
-            line = host_definition(device)
-            file.write(line)
+        file.write(generate_config())
 
 
 def get_devices():
-    devices = Device.object.all()
+    devices = Device.objects.all()
 
     return devices
 
 
-def host_definition(host):
-    mac = host.mac
-    line = 'host %s' % (mac)
-    return line
+def get_eui_mac(mac):
+    eui = EUI(mac)
+    eui.dialect = mac_unix_expanded
+    return str(eui)
+
+
+def host_definition(mac, model, ip):
+    # host host9 {hardware ethernet mac; fixed-address ip; }
+    eui = EUI(mac)
+    eui.dialect = mac_unix_expanded
+    ether = 'hardware ethernet %s' % get_eui_mac(mac)
+    ipaddr = 'fixed-address %s' % ip
+    config_line = 'host %s-%s {%s; %s;}\n' % (
+        model,
+        mac,
+        ether,
+        ipaddr,
+    )
+    return config_line
+
+
+def generate_config():
+    cnt = ''
+    # devices = Device.objects.all()
+    for device in get_devices():
+        mac = device.mac
+        model = device.dev
+        ip = device.ip
+        cnt += host_definition(mac=mac, model=model, ip=ip)
+
+    return cnt
