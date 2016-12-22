eeb502a5a9006bcabbd5810f48364817e642ad79
Piotr Dobrysiak <piotr.dobrysiak@gmail.com>
2016-12-22 15:54:03 +0100

    Added hosts config generator for DHCPd, fixed.

--- voiper/views/dhcp.py
+++ voiper/views/dhcp.py
@@ -1,6 +1,12 @@
 # -*- coding: UTF-8 -*-
 from django.views.generic import TemplateView
+from voiper.services.dhcp import generate_config
 
 
 class DhcpView(TemplateView):
     template_name = 'dhcp/view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DhcpView, self).get_context_data(**kwargs)
+        context['dhcp_file'] = generate_config()
+        return context
