3b744f5f66b132d811bc2bba64105b9ef977623c
Piotr Dobrysiak <piotr.dobrysiak@gmail.com>
2016-12-25 21:18:21 +0100

    Added images for django-tables2. Some fixed for table.

--- voiper/tables.py
+++ voiper/tables.py
@@ -1,8 +1,17 @@
 import django_tables2 as tables
 from voiper.models import Context
+import itertools
 
 
 class ContextTable(tables.Table):
+
+    def __init__(self, *args, **kwargs):
+        super(ContextTable, self).__init__(*args, **kwargs)
+        self.counter = itertools.count()
+
+    def render_row_number(self):
+        return 'Row %d' % next(self.counter)
+
     class Meta:
         model = Context
         # add class="paleblue" to <table> tag
